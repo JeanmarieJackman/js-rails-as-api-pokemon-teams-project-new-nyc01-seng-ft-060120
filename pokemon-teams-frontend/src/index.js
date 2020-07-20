6320460654154816d491ed55df5a7ce28fbc2979
DCR430 <daniel.c.reyes30@gmail.com>
2020-07-20 16:06:29 -0400

    second commit

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,81 @@
-const BASE_URL = "http://localhost:3000"
-const TRAINERS_URL = `${BASE_URL}/trainers`
-const POKEMONS_URL = `${BASE_URL}/pokemons`
+document.addEventListener("DOMContentLoaded",function(e){
+
+
+
+    const BASE_URL = "http://localhost:3000"
+    const TRAINERS_URL = `${BASE_URL}/trainers`
+    const POKEMONS_URL = `${BASE_URL}/pokemons`
+    const main = document.querySelector("main")
+    
+    
+    // When a user loads the page, they should see all trainers, 
+    // with their current team of Pokemon.
+    
+    getTrainers()
+    function getTrainers (){
+        fetch(TRAINERS_URL)
+        .then(res => res.json())
+        .then(trainers => renderTrainers(trainers)
+        
+        )}
+        
+        
+        
+        function renderTrainers(trainers){
+            trainers.forEach(element => {
+                 let trainerDiv = document.createElement("div") 
+                     trainerDiv.className = "card"
+                     trainerDiv.dataset.id = `${element.id}`
+                     trainerDiv.innerHTML =
+                     `<p> ${element.name}</p>
+                      <button data-trainer-id=${element.id}> Add Pokemon </button>
+                      <ul> </ul>
+                     `
+           
+
+
+            
+        
+        
+             console.log(trainerDiv)  
+        //  console.log(element["id"])
+        }) 
+     };
+        
+        
+        
+        // console.log(trainers[1].name)
+        
+    })
+    
+    // renderTrainers()
+    
+    
+    
+    // for (let i = 0; i < trainers.length; i++) {
+    //     trainerDiv.innerHTML = `
+    //          <p> ${trainers[i].name}</p>
+    //          <ul>
+    //         </ul>
+    //         `
+
+    //     }
+    //     main.append(trainerDiv)
+    
+// Whenever a user hits "Add Pokemon" and they have space on their team, 
+// they should get a new Pokemon.
+
+
+
+
+
+
+
+
+
+// Whenever a user hits "Release Pokemon" on a specific Pokemon team, 
+// that specific Pokemon should be released from the team.
+
+
+
+
